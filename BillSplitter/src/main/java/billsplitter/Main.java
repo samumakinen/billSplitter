9a25a283321f73fe9566da390d2cca0ba479fd60
Samu Mäkinen <samu.makin@gmail.com>
2020-04-20 00:24:53 +0300

    minor beautification

--- BillSplitter/src/main/java/billsplitter/Main.java
+++ BillSplitter/src/main/java/billsplitter/Main.java
@@ -1,10 +1,8 @@
 
 package billsplitter;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Launcher.main(args);
     }
 }
